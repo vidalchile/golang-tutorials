66dc9d97ae7bd7307ec0a2d3a4b452eb38c55e20
cristian vidal <cristian.vidal@autofact.cl>
2024-07-29 07:20:09 -0400

    sort string array

--- main.go
+++ main.go
@@ -1,36 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
 func main() {
-	var ages1 [3]int = [3]int{20, 25, 30}
-	var ages2 = [3]int{20, 25, 30}
+	greeting := "hello there friends!"
+	fmt.Println(strings.Contains(greeting, "hello"))
+	fmt.Println(strings.Contains(greeting, "bye"))
+	fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
+	fmt.Println(strings.ToUpper(greeting))
+	fmt.Println(strings.Index(greeting, "ll"))
+	fmt.Println(strings.Split(greeting, " "))
 
-	fmt.Println(ages1, ages2)
+	// the original value is unchanged
+	fmt.Println("original string value =", greeting)
 
-	names := [4]string{"yoshi", "mario", "peach", "bowser"}
-	names[1] = "luigi"
+	ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
 
+	sort.Ints(ages)
+	fmt.Println(ages)
+
+	index := sort.SearchInts(ages, 30)
+	fmt.Println(index)
+
+	names := []string{"yoshi", "mario", "peach", "bowser", "luigi"}
+
+	sort.Strings(names)
 	fmt.Println(names)
 
-	fmt.Println(ages2, len(ages2))
-	fmt.Println(names, len(names))
-
-	// slices (use arrays under the hood)
-	var scores = []int{100, 50, 60}
-	scores[2] = 25
-	scores = append(scores, 85)
-
-	fmt.Println(scores, len(scores))
-
-	// slice ranges
-	rangeOne := names[1:4] // doesn't include pos 4 element
-	rangeTwo := names[2:]  //includes the last element
-	rangeThree := names[:3]
-
-	fmt.Println(rangeOne, rangeTwo, rangeThree)
-	fmt.Printf("the type of rangeOne is %T \n", rangeOne)
-
-	rangeOne = append(rangeOne, "koopa")
-	fmt.Println(rangeOne)
+	fmt.Println(sort.SearchStrings(names, "bowser"))
 }
