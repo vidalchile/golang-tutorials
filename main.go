57deaac1dbd6e2a20c5592f962c57b0266ffb84c
cristian vidal <cristian.vidal@autofact.cl>
2024-08-19 08:25:47 -0400

    pass by value

--- main.go
+++ main.go
@@ -2,36 +2,55 @@ package main
 
 import "fmt"
 
+func updateName(n string) {
+	// no actualizamos la variable name
+	// actualizamos la copia de la variable name
+	n = "cristian"
+}
+
+func updateNameWithReturn(x string) string {
+	// no actualizamos la variable name
+	// actualizamos la copia de la variable name
+	x = "cristian"
+	return x
+}
+
+func updateMenu(y map[string]float64) {
+	y["coffee"] = 1.3
+}
+
 func main() {
+	// group A types -> strings, ints, bools, floats, arrays, structs (non-pointer values)
 
+	name := "carolina"
+
+	// al pasar una variable a una funcion go crea una copia
+	updateName(name)
+
+	fmt.Println(name)
+
+	// al crear una variable go la almacena en un bloque de memoria
+	/*
+
+		|----name----|------n-------|
+		|  0x001     |  0x002  	    |
+		|------------|--------------|
+		| "carolina" | "cristian"   |
+		|------------|--------------|
+
+	*/
+
+	// ¿Como solucionamos esto?
+	name = updateNameWithReturn(name)
+	println(name)
+
+	// group B types -> slices, maps, functions
 	menu := map[string]float64{
-		"soup":           10.1,
-		"pie":            10.2,
-		"salad":          10.3,
-		"toffee pudding": 10.4,
+		"pie":       1.1,
+		"ice cream": 1.2,
 	}
 
+	// para este tipo de caso si es posible modificar el valor original (pointer wrapper values)
+	updateMenu(menu)
 	fmt.Println(menu)
-
-	fmt.Println(menu["salad"])
-
-	// ints as key type
-	phoneBook := map[int]string{
-		267584967: "mario",
-		984759373: "luigi",
-		845775485: "peach",
-	}
-
-	for key, value := range phoneBook {
-		fmt.Println(key, "-", value)
-	}
-
-	fmt.Println(phoneBook)
-	fmt.Println(phoneBook[845775485])
-
-	phoneBook[984759373] = "bowser"
-	fmt.Println(phoneBook)
-
-	phoneBook[647583927] = "yoshi"
-	fmt.Println(phoneBook)
 }
