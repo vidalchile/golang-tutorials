07dde175755abf39f117bd70cb8c4aea972b7752
cristian vidal <cristian.vidal@autofact.cl>
2024-08-21 08:34:03 -0400

    Receiver Functions

--- main.go
+++ main.go
@@ -12,14 +12,30 @@ type bill struct {
 func newBill(name string) bill {
 	b := bill{
 		name:  name,
-		items: map[string]float64{},
+		items: map[string]float64{"pie": 5.99, "cake": 3.99},
 		tip:   0,
 	}
 	return b
 }
 
+func (b bill) format() string {
+	fs := "Bill breakpoint: \n"
+	var total float64 = 0
+
+	// list item
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25v ...$%v\n", k+":", v)
+		total += v
+	}
+
+	// add total
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total)
+
+	return fs
+}
+
 func main() {
 	myBill := newBill("mario's bill")
-
 	fmt.Println(myBill)
+	fmt.Println(myBill.format())
 }
