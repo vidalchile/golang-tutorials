449420f8d3f67120f8f8915b0af56ddfd3ae5ad3
cristian vidal <cristian.vidal@autofact.cl>
2024-08-27 07:48:01 -0400

    Receiver Functions with Pointers

---
+++ bill.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type bill struct {
+	name  string
+	items map[string]float64
+	tip   float64
+}
+
+// make new bills
+func newBill(name string) bill {
+	b := bill{
+		name:  name,
+		items: map[string]float64{},
+		tip:   0, // propina
+	}
+	return b
+}
+
+// add item to bill
+func (b *bill) addItem(name string, price float64) {
+	(*b).items[name] = price
+	// b.items[name] = price
+}
+
+func (b bill) format() string {
+	fs := "Bill breakpoint: \n"
+	var total float64 = 0
+
+	// list item
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25v ...$%v\n", k+":", v)
+		total += v
+	}
+
+	// add tip
+	fs += fmt.Sprintf("%-25v ...$%v\n", "tip:", b.tip)
+
+	// add total
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total+b.tip)
+
+	return fs
+}
+
+// update tip
+func (b *bill) updateTip(tip float64) {
+	(*b).tip = tip
+	// b.tip = tip
+}
--- main.go
+++ main.go
@@ -2,40 +2,16 @@ package main
 
 import "fmt"
 
-type bill struct {
-	name  string
-	items map[string]float64
-	tip   float64
-}
-
-// make new bills
-func newBill(name string) bill {
-	b := bill{
-		name:  name,
-		items: map[string]float64{"pie": 5.99, "cake": 3.99},
-		tip:   0,
-	}
-	return b
-}
-
-func (b bill) format() string {
-	fs := "Bill breakpoint: \n"
-	var total float64 = 0
-
-	// list item
-	for k, v := range b.items {
-		fs += fmt.Sprintf("%-25v ...$%v\n", k+":", v)
-		total += v
-	}
-
-	// add total
-	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total)
-
-	return fs
-}
-
 func main() {
 	myBill := newBill("mario's bill")
+
+	myBill.updateTip(10)
+
+	myBill.addItem("onion soup", 4.50)
+	myBill.addItem("veg pie", 8.95)
+	myBill.addItem("toffee pudding", 4.95)
+	myBill.addItem("coffee", 3.25)
+
 	fmt.Println(myBill)
 	fmt.Println(myBill.format())
 }
